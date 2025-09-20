b601806256a131cd69350f0a10a9516075c80259
Leonardo Marcoski <47371084+Sooyz@users.noreply.github.com>
2025-09-20 04:30:23 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,24 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const versionResponse = await database.query("SHOW server_version");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const opennedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   response.status(200).json({
-    chave: "balão",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionResponse.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        openned_connections: parseInt(opennedConnections.rows[0].count),
+      },
+    },
   });
 }
 
